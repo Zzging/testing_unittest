25716be275d41dcfb4bf42d792a35c1c9a33d6fe
Zzging <chanikan.po@mail.wu.ac.th>
2020-09-28 11:57:16 +0700

    new

--- src/test/java/com/unittest/testing/AppTest.java
+++ src/test/java/com/unittest/testing/AppTest.java
@@ -163,117 +163,132 @@ public class AppTest
 		assertEquals(expected,actual);
 	}
 
-//		@Test
-//		public void testResult0input0() {
-//			double input = 0;
-//			double expected =0;
-//			double actual = object.input(input);
-//			assertEquals(expected,actual);
-//		}
-//		@Test
-//		public void testResult0input1() {
-//			double input = 1;
-//			double expected =33.8;
-//			double actual = object.input(input);
-//			assertEquals(expected,actual);
-//		}
-//		@Test
-//		public void testResult0inputNegative1() {
-//			double input = -1;
-//			double expected = 30.2;
-//			double actual = object.input(input);
-//			assertEquals(expected,actual);
-//		}
+		@Test
+		public void testResult00input00() {
+			double input = 0;
+			double expected =0;
+			object.input(input);
+			double actual = object.getCelcius();
+			assertEquals(expected,actual,2);
+		}
+		@Test
+		public void testResult01input01() {
+			double input = 1;
+			double expected =1;
+			object.input(input);
+			double actual = object.getCelcius();
+			assertEquals(expected,actual,2);
+		}
+		@Test
+		public void testResult00inputNegative01() {
+			double input = -1;
+			double expected = -1;
+			object.input(input);
+			double actual = object.getCelcius();
+			assertEquals(expected,actual,2);
+		}
 //		@Test
 //		public void testResult0input499() {
 //			double input = 499;
 //			double expected = 930.2;
-//			double actual = object.input(input);
-//			assertEquals(expected,actual);
+//			object.input(input);
+//			double actual = object.getCelcius();
+//			assertEquals(expected,actual,2);
 //		}
-////		@Test
-////		public void testResult837inputNegative499() {
-////			double input = 499;
-////			double expected = -837.2;
-////			double actual = object.input(input);
-////			assertEquals(expected,actual);
-////		}
+//		@Test
+//		public void testResult499inputNegative499() {
+//			double input = 499;
+//			double expected = -837.2;
+//			object.input(input);
+//			double actual = object.getCelcius();
+//			assertEquals(expected,actual,2);
+//		}
 //		@Test
 //		public void testResult0input500() {
 //			double input = 500;
 //			double expected = 932;
-//			double actual = object.input(input);
-//			assertEquals(expected,actual);
+//			object.input(input);
+//			double actual = object.getCelcius();
+//			assertEquals(expected,actual,2);
 //		}
 //		@Test
 //		public void testResult0inputNegative500() {
 //			double input = -500;
 //			double expected = -868;
-//			double actual = object.input(input);
-//			assertEquals(expected,actual);
+//			object.input(input);
+//			double actual = object.getCelcius();
+//			assertEquals(expected,actual,2);
 //		}
 //		@Test
 //		public void testResult0input501() {
 //			double input = 501;
 //			double expected = 933.8;
-//			double actual = object.input(input);
-//			assertEquals(expected,actual);
+//			object.input(input);
+//			double actual = object.getCelcius();
+//			assertEquals(expected,actual,2);
 //		}
 //		@Test
 //		public void testResult0inputNegative501() {
 //			double input = -501;
 //			double expected = -869.8;
-//			double actual = object.input(input);
-//			assertEquals(expected,actual);
+//			object.input(input);
+//			double actual = object.getCelcius();
+//			assertEquals(expected,actual,2);
 //		}
 //		@Test
 //		public void testResult0input999() {
 //			double input = 999;
 //			double expected = 1830.2;
-//			double actual = object.input(input);
-//			assertEquals(expected,actual);
+//			object.input(input);
+//			double actual = object.getCelcius();
+//			assertEquals(expected,actual,2);
 //		}
 //		@Test
 //		public void testResult0inputNegative999() {
 //			double input = -999;
 //			double expected = -1766.2;
-//			double actual = object.input(input);
-//			assertEquals(expected,actual);
+//			object.input(input);
+//			double actual = object.getCelcius();
+//			assertEquals(expected,actual,2);
 //		}
 //		@Test
 //		public void testResult0input1001() {
 //			double input = 1001;
 //			double expected = 1833.8;
-//			double actual = object.input(input);
-//			assertEquals(expected,actual);
+//			object.input(input);
+//			double actual = object.getCelcius();
+//			assertEquals(expected,actual,2);
 //		}
 //		@Test
 //		public void testResult0inputNegative1001() {
 //			double input = -1;
 //			double expected = -1769.8;
-//			double actual = object.input(input);
-//			assertEquals(expected,actual);
+//			object.input(input);
+//			double actual = object.getCelcius();
+//			assertEquals(expected,actual,2);
 //		}
 //		@Test
 //		public void testResult0input1010() {
 //			double input = 1010;
 //			double expected = 1850;
-//			double actual = object.input(input);
-//			assertEquals(expected,actual);
+//			object.input(input);
+//			double actual = object.getCelcius();
+//			assertEquals(expected,actual,2);
 //		}
 //		@Test
 //		public void testResult0input2000() {
 //			double input = 2000;
 //			double expected = 3632;
-//			double actual = object.input(input);
-//			assertEquals(expected,actual);
-//		}
+//			object.input(input);
+//			double actual = object.getCelcius();
+//			assertEquals(expected,actual,2);
 //		@Test
-//		public void testResult0inputNegative2000() {
+//		public void testResult2000inputNegative2000() {
 //			double input = -2000;
 //			double expected = -3568;
-//			double actual = object.input(input);
-//			assertEquals(expected,actual);
+//			object.input(input);
+//			double actual = object.getCelcius();
+//			assertEquals(expected,actual,2);
 //		}
-//	}	
+		
 }
